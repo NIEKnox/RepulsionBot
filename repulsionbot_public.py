a6dcf8441d0693218cbd67bc738a4bd3ad6ec055
NIEKnox <noah.ie.knox@gmail.com>
2021-03-10 00:14:55 +0000

    Updated Repulsionbot; improved encrypt function and added bug functionality.
    Encrypt function now no longer requires quotes around the sent message text, and also checks for any bugs placed on the sender or receiver. 
    
    Added bug functionality integrated with nicknames database:
    'Bugs' can now be placed on another player, with the caller of the function being designated the setter, and by default give 3 uses. Bugs will send encrypted messages that are sent or received by the person the bug was placed on to the setter.
    Ref functionality added to the bugs, with the ability to: 
    - View all listed bugs.
    - Place bugs for another person with a designated number of uses.
    - Delete specific bugs.
    - Purge all bugs.
    If someone who is not a ref tries to access these functions, it will not allow it, and can send a message to a designated channel with a note of who attempted to call the function.

--- repulsionbot_public.py
+++ repulsionbot_public.py
@@ -1,8 +1,5 @@
-import os
 import sqlite3
-import random
 import uuid
-import datetime
 
 import discord
 from discord.ext import commands
@@ -14,7 +11,10 @@ bot = commands.Bot(command_prefix=';')
 encryption_log = "INSERT CHANNELID TO PRINT ENCRYPTED MESSAGES TO HERE"
 bot_actions = "INSERT CHANNELID TO PRINT ALL BOT ACTIONS TO HERE"
 
+ref1 = "INSERT USER ID OF REF 1 IN HERE" # Comment as to who this number corresponds to
+ref2 = "INSERT USER ID OF REF 2 IN HERE" # Comment as to who this number corresponds to
 
+refs = [ref1, ref2] # Refs can be added to this list indefinitely
 
 @bot.event
 async def on_ready():
@@ -26,33 +26,76 @@ async def on_ready():
 
 
 @bot.command(name='encrypt', help='Can be used to send encrypted messages to another player. '
-                                  'Format messages as ;encrypt recipient_ID "message goes here, including the quotes".')
-async def encrypt(ctx, sendee, sent_text):
-    message = ctx.message
-    sender = message.author
+                                  'Format messages as ;encrypt recipient_nickname "message goes here, not necessarily the quotes".')
+async def encrypt(ctx, *sent_text):
+    if len(sent_text) == 0:
+        await ctx.send("Not enough data sent!")
+        return
+    if len(sent_text) == 1:
+        await ctx.send("You must send something to this user!")
+        return
+    sendee = sent_text[0]
+    encrypted_message = ' '.join(sent_text[1::])
+    sender = ctx.author
     sendeeLower = sendee.lower()
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
     c.execute("""SELECT * FROM playerIDs WHERE identifier = ?""", (sendeeLower,))
     nickname = c.fetchone()
-
     if nickname != None:
-        user = bot.get_user(int(nickname[1]))
+        user_id = int(nickname[1])
+        user = await bot.fetch_user(user_id)
     else:
         await ctx.send("Oops! Can't find recipient. Make sure they're in the database, or contact a ref for help.")
         return
     encryptID = str(uuid.uuid1())
     sendee_channel = await user.create_dm()
-    sender_channel = await sender.create_dm()
     channel_el = bot.get_channel(encryption_log)
     await sendee_channel.send("You have received an encrypted message on channel " + encryptID + ":" + "\n" +
-                              sent_text)
-    await sender_channel.send("You have successfully sent a message on encrypted channel " + encryptID + ":" + "\n" +
-                              sent_text)
+                              encrypted_message)
+    await ctx.send("You have successfully sent a message on encrypted channel " + encryptID + ":" + "\n" +
+                              encrypted_message)
     # print(channel_el)
-    await channel_el.send(str(sender) + " sent a message to " + str(user) + ":" + "\n" + sent_text)
-    print(sender, "sent a message to", user, ":", sent_text)
-    # await bot.send_message(sent_text)
+    await channel_el.send(str(sender) + " sent a message to " + str(user) + ":" + "\n" + encrypted_message)
+    print(sender, "sent a message to", user, ":", encrypted_message)
+
+    c.execute("""SELECT * FROM bugs WHERE target = ?""", (str(sender.id),))
+    sender_rows = c.fetchall()
+    if sender_rows != []:
+        for row in sender_rows:
+            bug_id = row[0]
+            placer_id = int(row[2])
+            activations = int(row[3])
+            new_activations = activations-1
+            placer = await bot.fetch_user(placer_id)
+            await placer.send("A bug you placed on " + str(sender) + " has picked up a message:\n" + encrypted_message + "\nYour bug has "
+                              + str(new_activations) + " uses left!")
+            if new_activations <= 0:
+                c.execute("""DELETE FROM bugs WHERE bugID = ?""", (bug_id,))
+
+            else:
+                c.execute('''UPDATE bugs SET activations = ? WHERE bugID = ?''', (new_activations, bug_id,))
+
+    c.execute("""SELECT * FROM bugs WHERE target = ?""", (str(user_id),))
+    receiver_rows = c.fetchall()
+    # print(receiver_rows)
+    if receiver_rows != []:
+        for row in receiver_rows:
+            bug_id = row[0]
+            placer_id = int(row[2])
+            activations = int(row[3])
+            new_activations = activations-1
+            placer = await bot.fetch_user(placer_id)
+            await placer.send("A bug you placed on " + str(user) + "has picked up a message:\n" + encrypted_message + "\nYour bug has "
+                              + str(new_activations) + " uses left!")
+            if new_activations <= 0:
+                c.execute("""DELETE FROM bugs WHERE bugID = ?""", (bug_id,))
+
+            else:
+                c.execute('''UPDATE bugs SET activations = ? WHERE bugID = ?''', (str(new_activations), bug_id,))
+
+    conn.commit()
+    conn.close()
 
 @bot.command(name='add_nick', help='Adds nicknames for players to be used in ;encrypt instead of their Discord IDs.')
 async def add_nick(ctx, nickname, userID):
@@ -119,6 +162,109 @@ async def nick_player(ctx, user_ID):
     for row in rows:
         text += row[0] + ', '
     await ctx.send(text)
+    conn.close()
+
+@bot.command(name='place_bug', help='Places a bug on another player. Format using ;place_bug target, where target can be the nickname of a target.')
+async def place_bug(ctx, nickname):
+    setter = ctx.author
+    setter_id = setter.id
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("""SELECT userID FROM playerIDs WHERE identifier = ?""", (nickname,))
+    existing_bug = c.fetchone()
+    target_id = existing_bug[0]
+    c.execute("""SELECT * FROM bugs WHERE target = ? AND setter = ?""", (str(target_id),str(setter_id),))
+    row = c.fetchone()
+    print(row)
+    if row != None:
+        await ctx.send("You have already placed a bug on this player! It has sent " + str((3 - int(row[3]))) + " messages!")
+    else:
+        bugID = str(uuid.uuid1())
+        print(str(target_id), str(setter_id))
+        c.execute("""INSERT INTO bugs VALUES (?, ?, ?, ?)""", (str(bugID), str(target_id), str(setter_id), 3,))
+        conn.commit()
+        await ctx.send("You have successfully placed a bug on " + nickname + "!")
+    conn.close()
+
+@bot.command(name='bugs', help='Lists all placed bugs. FOR REF USE ONLY!')
+async def bugs(ctx):
+    user = ctx.author
+    user_id = user.id
+    channel_ba = bot.get_channel(bot_actions)
+    if user_id not in refs:
+        await ctx.send("You are not authorised to execute this command!")
+        await channel_ba.send("Player " + str(user_id) + " just tried to access bugs!")
+        return
+    print("Bugs just happened!")
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("""SELECT * FROM bugs""")
+    rows = c.fetchall()
+    text = "Here are all of the currently active bugs:\n"
+    for row in rows:
+        text += 'Bug ' + row[0] + ': ' + row[2] + ' has placed a bug on ' + row[1] + '. It has ' + str(row[3]) + ' uses left!\n'
+    await ctx.send(text)
+    conn.close()
+
+@bot.command(name='purge_bugs', help='Removes all placed bugs. FOR REF USE ONLY!')
+async def purge_bugs(ctx):
+    user = ctx.author
+    user_id = user.id
+    channel_ba = bot.get_channel(bot_actions)
+    if user_id not in refs:
+        await ctx.send("You are not authorised to execute this command!")
+        await channel_ba.send("Player " + str(user_id) + " just tried to purge all bugs!")
+        return
+    print("Purge bugs just happened!")
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("""DELETE FROM bugs""")
+    await ctx.send("All bugs have been purged. Long live the debuggers.")
+    conn.commit()
+    conn.close()
+
+@bot.command(name='place_bug_ref', help='Allows the manual setting of bugs. Format ;place_bug_ref target setter number of uses. FOR REF USE ONLY!')
+async def place_bug_ref(ctx, target, setter, uses):
+    user = ctx.author
+    user_id = user.id
+    channel_ba = bot.get_channel(bot_actions)
+    if user_id not in refs:
+        await ctx.send("You are not authorised to execute this command!")
+        await channel_ba.send("Player " + str(user_id) + " just tried to purge all bugs!")
+        return
+    print("Place bug ref just happened!")
+    bugID = str(uuid.uuid1())
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("""SELECT userID FROM playerIDs WHERE identifier = ?""", (target.lower(),))
+    target_row = c.fetchone()
+    target_id = target_row[0]
+    c.execute("""SELECT userID FROM playerIDs WHERE identifier = ?""", (setter.lower(),))
+    setter_row = c.fetchone()
+    setter_id = setter_row[0]
+    c.execute("""INSERT INTO bugs VALUES (?, ?, ?, ?)""", (str(bugID), str(target_id), str(setter_id), str(uses),))
+    await ctx.send("A bug has been manually placed on " + str(target) + ", and will now send messages " + str(uses)
+                          + " times to " + str(setter) + ". Thank you for your service, Ref!")
+    await channel_ba.send("A bug has been manually placed on " + str(target) + ", and will now send messages " + str(uses)
+                          + " times to " + str(setter) + ".")
+    conn.commit()
+    conn.close()
+
+@bot.command(name='remove_bug', help='Removes a specific placed bug. FOR REF USE ONLY!')
+async def remove_bug(ctx, bug_id):
+    user = ctx.author
+    user_id = user.id
+    channel_ba = bot.get_channel(bot_actions)
+    if user_id not in refs:
+        await ctx.send("You are not authorised to execute this command!")
+        await channel_ba.send("Player " + str(user_id) + " just tried to remove a bug!")
+        return
+    print("Remove bug just happened!")
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("""DELETE FROM bugs WHERE bugID=?""", (str(bug_id),))
+    await ctx.send("A bug has been purged. Please press F.")
+    conn.commit()
     conn.close()
 
 print("Starting bot")
